b715f9d9d102fdeb45432a86ce3745dcbccd07e4
ankitkumar8258 <baghelankit8258@gmail.com>
2026-04-21 09:49:37 +0530

    Add files via upload

---
+++ Team case stduy.cpp
@@ -0,0 +1,92 @@
+#include<iostream>
+using namespace std;
+  class Group1
+  {
+  	public:
+  		char name;
+  		int Score;
+  		void T1()
+  		{
+  			cout<<"enter the team name"<<endl;
+  			cin>> name;
+  			cout<<" enter the team Score"<<endl;
+  			cin>> Score;
+		  }
+		  void T2()
+  		{
+  			cout<<"enter the team name"<<endl;
+  			cin>> name;
+  			cout<<" enter the team Score"<<endl;
+  			cin>> Score;
+		  }
+		  void T3()
+  		{
+  			cout<<"enter the team name"<<endl;
+  			cin>> name;
+  			cout<<" enter the team Score"<<endl;
+  			cin>> Score;
+		  }
+		  void T4()
+  		{
+  			cout<<"enter the team name"<<endl;
+  			cin>> name;
+  			cout<<" enter the team Score"<<endl;
+  			cin>> Score;
+		  }
+		  void T5()
+  		{
+  			cout<<"enter the team name"<<endl;
+  			cin>> name;
+  			cout<<" enter the team Score"<<endl;
+  			cin>> Score;
+		  }
+		  void T6()
+  		{
+  			cout<<"enter the team name"<<endl;
+  			cin>> name;
+  			cout<<" enter the team Score"<<endl;
+  			cin>> Score;
+		  }
+		  void T7()
+  		{
+  			cout<<"enter the team name"<<endl;
+  			cin>> name;
+  			cout<<" enter the team Score"<<endl;
+  			cin>> Score;
+		  }
+		  void T8()
+  		{
+  			cout<<"enter the team name"<<endl;
+  			cin>> name;
+  			cout<<" enter the team Score"<<endl;
+  			cin>> Score;
+		  }
+
+
+  };
+  class Group2: public Group1{
+  	public:
+  		void set
+  		{
+
+  		if(Score>100)
+  		Cout<<"team in Group1"<<endl;
+  		else
+  		cout<<"team in group 2"<<endl;
+  	}
+
+  };
+  int main
+  {
+  	Group2 b;
+  	b.t1();
+  	b.t2();
+  	b.t3();
+  	b.t4();
+  	b.t5();
+  	b.t6();
+  	b.t7();
+  	b.t8();
+  	b.set();
+  	return 0;
+  }
